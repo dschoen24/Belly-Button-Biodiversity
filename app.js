6f51e13a6dad6686809055aca90bc1fd716b525c
Debra Potts <dschoenstl24@gmail.com>
2021-08-22 11:41:20 -0500

    updated code

--- app.js
+++ app.js
@@ -1,30 +1,82 @@
 // create the function that gets the data and creates the plots for the id 
-// function getPlot(id) {
+function buildMetadata(selection) {
     
-        // get the data from the json file
-        // d3.json("data/samples.json").then((data)=> {
-        //     console.log(data)
+        // Read the data from the json file
+        d3.json("data/samples.json").then((sampleData)=> {
+
+            console.log(sampleData)
     
-        //     var wfreq = data.metadata.map(d => d.wfreq)
-        //     console.log(`Washing Freq: ${wfreq}`)
+            // Parse and filter data to get metadata
+            let parsedData = sampleData.metadata;
+            console.log("parsed data inside buildMetadata function")
+            console.log(parsedData);
     
-            // filter sample values by id 
-            // var samples = data.samples.filter(s => s.id.toString() === id)[0];
+            let samples = parsedData.filter(item => item.id === selection);
+            console.log("showing sample[0]:");
+            console.log(sample[0]);
     
-            // console.log(samples);
-    
-            // get only top 10 sample values to plot and reverse for the plotly
-            let sampleValues = samples.sample_values.slice(0, 10).reverse();
-            console.log(sampleValues)
-    
-            // get only top 10 otu ids for the plot
-            let idValues = samples.otu_ids.slice(0, 10).reverse();
-            console.log(idValues)
+            // Get location of metadata & update it
+            let metadata = d3.select("sample-metadata").html("");
+
+            Object.entries(sample[0].foreach(([key, value]) => {
+                metadata.append("p").text(`${key}: ${value}`);
+            });
+
+            console.log("next again");
+            console.log(metadata);
+        });
+    }
+
+    // Call in function that will create charts for the given sample
+    function buildCharts(selection) {
+
+        //Read the json data
+        d3.json("data/samples.json").then((sampleData)=> {
+
+            // Parse & filter the data to get the sample's OTU data
+            let parsedData = sampleData.samples;
+            console.log("parsed data inside buildCharts function")
+            console.log(parsedData);
+
+            let sampleDict = parsedData.filter(item => item.id == selection)[0];
+            console.log("sampleDict")
+            console.log(sampleDict);
+
+            let sampleValues = sampleDict.sample_values;
+            let barChartValues = sampleValues.slice(0, 10).reverse();
+            console.log("sample_values")
+            console.log(barChartValues);
+
+            let idValues = sampleDict.otu_ids;
+            let barChartLabels = idValues.slice(0, 10).reverse();
+            console.log("otu_ids");
+            console.log(barChartLabels);
+
+            let reformattedLabels = [];
+            barChartLabels.forEach((label) => {
+                reformattedLabels.push("OTU" + label);
+            });
+
+            console.log("reformatted");
+            console.log(reformattedLabels);
+
             
-            // get the otu id's to the desired form for the plot
-            let otu_ids = idValues.map(d => "OTU" + d)
+
+
+        }
+    }
+
+            // let sampleValues = samples.sample_values.slice(0, 10).reverse();
+            // console.log(sampleValues)
     
-            console.log(otu_ids)
+            // // get only top 10 otu ids for the plot
+            // let idValues = samples.otu_ids.slice(0, 10).reverse();
+            // console.log(idValues)
+            
+            // // get the otu id's to the desired form for the plot
+            // let otu_ids = idValues.map(d => "OTU" + d)
+    
+            // console.log(otu_ids)
     
             // get the top 10 labels for the plot
             let labels = samples.otu_labels.slice(0, 10);
