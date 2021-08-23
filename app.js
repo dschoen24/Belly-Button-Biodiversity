e50c99f1c872c5a78f2f60d723912a81cc6ba916
Debra Potts <dschoenstl24@gmail.com>
2021-08-22 20:08:08 -0500

    update

--- app.js
+++ app.js
@@ -19,115 +19,114 @@ function buildMetadata(selection) {
         let metadata = d3.select("sample-metadata").html("");
 
         Object.entries(sample[0].foreach(([key, value]) => {
-            metadata.append("p").text(`${key}: ${value}`);
-        })
+            metadata.append("p").text(`${key}: ${value}`)
+        }),
 
-        console.log("next again"),
-        console.log(metadata)
-    );
-}
+        console.log("next again"));
+        console.log(metadata);
+    },
 
 // Call in function that will create charts for the given sample
 function buildCharts(selection) {
 
-    //Read the json data
-    d3.json("data/samples.json").then((sampleData) => {
+            //Read the json data
+            d3.json("data/samples.json").then((sampleData) => {
 
-        // Parse & filter the data to get the sample's OTU data
-        let parsedData = sampleData.samples;
-        console.log("parsed data inside buildCharts function")
-        console.log(parsedData);
+                // Parse & filter the data to get the sample's OTU data
+                let parsedData = sampleData.samples;
+                console.log("parsed data inside buildCharts function")
+                console.log(parsedData);
 
-        let sampleDict = parsedData.filter(item => item.id == selection)[0];
-        console.log("sampleDict")
-        console.log(sampleDict);
+                let sampleDict = parsedData.filter(item => item.id == selection)[0];
+                console.log("sampleDict")
+                console.log(sampleDict);
 
-        let sampleValues = sampleDict.sample_values;
-        let barChartValues = sampleValues.slice(0, 10).reverse();
-        console.log("sample_values")
-        console.log(barChartValues);
+                let sampleValues = sampleDict.sample_values;
+                let barChartValues = sampleValues.slice(0, 10).reverse();
+                console.log("sample_values")
+                console.log(barChartValues);
 
-        let idValues = sampleDict.otu_ids;
-        let barChartLabels = idValues.slice(0, 10).reverse();
-        console.log("otu_ids");
-        console.log(barChartLabels);
+                let idValues = sampleDict.otu_ids;
+                let barChartLabels = idValues.slice(0, 10).reverse();
+                console.log("otu_ids");
+                console.log(barChartLabels);
 
-        let reformattedLabels = [];
-        barChartLabels.forEach((label) => {
-            reformattedLabels.push("OTU" + label);
-        });
+                let reformattedLabels = [];
+                barChartLabels.forEach((label) => {
+                    reformattedLabels.push("OTU" + label);
+                });
 
-        console.log("reformatted");
-        console.log(reformattedLabels);
+                console.log("reformatted");
+                console.log(reformattedLabels);
 
-        let hovertext = sampleDict.otu_labels;
-        let barCharthovertext = hovertext.slice(0, 10).reverse();
-        console.log("otu_labels");
-        console.log(barCharthovertext);
+                let hovertext = sampleDict.otu_labels;
+                let barCharthovertext = hovertext.slice(0, 10).reverse();
+                console.log("otu_labels");
+                console.log(barCharthovertext);
 
-        // Create Bar Chart
-        let barChartTrace = {
-            type: "bar",
-            y: reformattedLabels,
-            x: barChartValues,
-            text: barCharthovertext,
-            orientation: "h"
-        };
+                // Create Bar Chart
+                let barChartTrace = {
+                    type: "bar",
+                    y: reformattedLabels,
+                    x: barChartValues,
+                    text: barCharthovertext,
+                    orientation: "h"
+                };
 
-        let barChartData = [barChartTrace];
+                let barChartData = [barChartTrace];
 
-        Plotly.newPlot("bar", barChartTrace);
+                Plotly.newPlot("bar", barChartTrace);
 
-        //Create the Bubble Chart
+                //Create the Bubble Chart
 
-        let bubbleChartTrace = {
-            x: idValues,
-            y: sampleValues,
-            text: hovertext,
-            mode: "markers",
-            marker: {
-                color: idValues,
-                    size: sampleValues
-            }
-        };
+                let bubbleChartTrace = {
+                    x: idValues,
+                    y: sampleValues,
+                    text: hovertext,
+                    mode: "markers",
+                    marker: {
+                        color: idValues,
+                        size: sampleValues
+                    }
+                };
 
-        let bubbleChartData = [bubbleChartTrace];
+                let bubbleChartData = [bubbleChartTrace];
 
-        let layout = {
-            showlegend: false,
-            height: 600,
-            width: 1000,
-            xaxis: {
-                title: "OTU ID"
-            }
-        };
+                let layout = {
+                    showlegend: false,
+                    height: 600,
+                    width: 1000,
+                    xaxis: {
+                        title: "OTU ID"
+                    }
+                };
 
-        Plotly.newPlot("bubble", bubbleChartData, layout);
+                Plotly.newPlot("bubble", bubbleChartData, layout);
 
 
-    });
-}
+            });
+        }
 
 // Define function that will run on page load
 function init() {
-    //Read json data
-    d3.json("data/samples.json").then((sampleData) => {
+            //Read json data
+            d3.json("data/samples.json").then((sampleData) => {
 
-        // Parse & Filter data to get sample names
-        let parsedData = sampleData.names;
-        console.log("parsed data inside init function")
-        console.log(parsedData);
+                // Parse & Filter data to get sample names
+                let parsedData = sampleData.names;
+                console.log("parsed data inside init function")
+                console.log(parsedData);
 
-        //Add Dropdown option for each sample
-        let dropdownMenu.append("option").property("value", name).text(name);
-    })
+                //Add Dropdown option for each sample
+                let dropdownMenu.append("option").property("value", name).text(name);
+            }),
 
-    // Use sample to build metadata & initial plots
-    buildMetadata(parsedData[0]);
+            // Use sample to build metadata & initial plots
+            buildMetadata(parsedData[0]);
 
-    buildCharts(parsedData[0]);
+            buildCharts(parsedData[0]);
 
-});
+        });
 }
 
 function optionChanged(newSelection) {
